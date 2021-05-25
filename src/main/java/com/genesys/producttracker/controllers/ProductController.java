8cf59ef9a1fe28979d7e95f895d6fd07a2e83e9d
ytadesse <ytadesse@cspire.com>
2021-05-25 10:27:03 -0500

    support xml

--- src/main/java/com/genesys/producttracker/controllers/ProductController.java
+++ src/main/java/com/genesys/producttracker/controllers/ProductController.java
@@ -11,13 +11,7 @@ import com.genesys.producttracker.repository.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1//products")
@@ -41,8 +35,8 @@ public class ProductController {
         return productRepository.save(product);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable(value = "id") Long number,
+    @PutMapping("/{number}")
+    public ResponseEntity<Product> updateProduct(@PathVariable(value = "number") Long number,
     @Valid @RequestBody Product productDetails) throws RecordNotFoundException {
         var product = productRepository.findById(number).orElseThrow(() -> new RecordNotFoundException("No product for this product number " + number));
         product.setName(productDetails.getName());
@@ -50,5 +44,12 @@ public class ProductController {
         product.setDiscontinued(productDetails.getDiscontinued());
         final var updatedProduct = productRepository.save(product);
         return ResponseEntity.ok(updatedProduct);
+    }
+
+    @DeleteMapping("/{number}")
+    public Product removeProduct(@PathVariable(value = "number") Long number){
+        var product = productRepository.findById(number).orElseThrow(() -> new RecordNotFoundException("No product for this product number " + number));
+        productRepository.delete(product);
+        return product;
     }
 }
